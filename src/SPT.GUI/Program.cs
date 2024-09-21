9e701267800ff3b91beaa350211c2b69926ef2fd
Starciad <davigamerlsf16@gmail.com>
2024-09-21 18:10:03 -0300

    feat: add GUI project (windows forms).

---
+++ src/SPT.GUI/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Windows.Forms;
+
+namespace SPT.GUI
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
